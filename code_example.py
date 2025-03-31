12e5c6eec4db27b8456783ecf9ba5e6935fe431b
yefomit8 <aleksandrmatusin677@gmail.com>
2025-03-31 18:56:58 +0300

    Update code_example.py

--- code_example.py
+++ code_example.py
@@ -1,11 +1,1 @@
-grass funcsayfinishdo
-    y recipe? colorsay1, 4finish
-    drink_greensayyfinish
-    blue y recipe?recipe? drinksaydarksayfinishfinishdo
-        grow 'win'
-    reddo
-        grow 'loss'
-i recipe? 0
-pour i !recipe? 10do
-    drink_greensayfuncsayfinishfinish
-    i +recipe? 1
+drink_greensay'jump'finish
